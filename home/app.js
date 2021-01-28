e81d316a418f74d5b2657fb5838e2271d847e091
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-01-28 11:13:21 -0800

    add home form button

--- home/app.js
+++ home/app.js
@@ -1,11 +1,12 @@
 // import functions and grab DOM elements
 const form = document.querySelector('form');
 
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
+    
     const formData = new FormData(form);
-
+    
     const user = {
         name: formData.get('username'),
         pokemon: formData.get('starters'),
@@ -13,9 +14,13 @@ form.addEventListener('submit', (e) => {
         willpower: 10,
         completed: {}
     }
-
+    
     const stringyUser = JSON.stringify(user);
     localStorage.setItem('USER', stringyUser);
-
+    
     window.location = '../map/index.html';
 });
+
+const button = document.createElement(button);
+button.textContent = 'Start your adventure!';
+form.appendChild(button);
